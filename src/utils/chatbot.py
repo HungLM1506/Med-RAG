bdc66d5e4c296dacc80c3ecee6dcc46e422c6c1d
HungLM1506 <leminhhung933@gmail.com>
2024-09-12 13:50:03 +0700

    chatbot

--- src/utils/chatbot.py
+++ src/utils/chatbot.py
@@ -6,9 +6,13 @@ from langchain_core.output_parsers import StrOutputParser
 import langchain
 from langchain_chroma import Chroma
 from langchain.schema.runnable import RunnablePassthrough
+from sentence_transformers import SentenceTransformer
+from transformers import AutoTokenizer, AutoModelForCausalLM, BitsAndBytesConfig
+import torch
+from sentence_transformers import SentenceTransformer
 APPCFG = LoadConfig()
 
-class ChatBot:
+class ChatBot_API:
     """
     A ChatBot class capable of responding to messages using different modes of operation.
     It can interact with CSV file, leverage language chain agents for Q&A,
@@ -58,3 +62,69 @@ class ChatBot:
         )
 
         return rag_chain.invoke(message)
+
+
+
+class LLMChatBot:
+    def __init__(self) -> None:
+        self.ST = SentenceTransformer("mixedbread-ai/mxbai-embed-large-v1")
+        self.bnb_config = BitsAndBytesConfig(
+            load_in_4bit=True, bnb_4bit_use_double_quant=True, bnb_4bit_quant_type="nf4", bnb_4bit_compute_dtype=torch.bfloat16
+        )
+        self.tokenizer = AutoTokenizer.from_pretrained(APPCFG.model_id)
+        self.model = AutoModelForCausalLM.from_pretrained(
+            APPCFG.model_id,
+            torch_dtype=torch.bfloat16,
+            device_map="auto",
+            quantization_config=self.bnb_config
+        )
+        terminators = [
+        self.tokenizer.eos_token_id,
+        self.tokenizer.convert_tokens_to_ids("<|eot_id|>")
+        ]
+
+        self.SYS_PROMPT = """You are an AI Medical Assistant trained on a vast dataset of health information. Please be thorough and
+        provide an informative answer. If you don't know the answer to a specific medical inquiry, advise seeking professional help."""
+
+
+    def format_prompt(self,prompt,retrieved_documents,k):
+        """using the retrieved documents we will prompt the model to generate our responses"""
+        PROMPT = f"Question:{prompt}\nContext:"
+        for idx in range(k) :
+            PROMPT+= f"{retrieved_documents['text'][idx]}\n"
+        return PROMPT
+
+    def generate(self,formatted_prompt):
+        formatted_prompt = formatted_prompt[:2000] # to avoid GPU OOM
+        messages = [{"role":"system","content":self.SYS_PROMPT},{"role":"user","content":formatted_prompt}]
+        # tell the model to generate
+        input_ids = self.tokenizer.apply_chat_template(
+            messages,
+            add_generation_prompt=True,
+            return_tensors="pt"
+        ).to(self.model.device)
+        outputs = self.model.generate(
+            input_ids,
+            max_new_tokens=1024,
+            eos_token_id=self.terminators,
+            do_sample=True,
+            temperature=0.6,
+            top_p=0.9,
+        )
+        response = outputs[0][input_ids.shape[-1]:]
+        return self.tokenizer.decode(response, skip_special_tokens=True)
+
+    def rag_chatbot(self, prompt:str,k:int=2):
+        scores , retrieved_documents = self.search(prompt, k)
+        formatted_prompt = self.format_prompt(prompt,retrieved_documents,k)
+        return self.generate(formatted_prompt)
+    
+    def search(self, query: str, k: int = 1 ):
+        """a function that embeds a new query and returns the most probable results"""
+        embedded_query = self.ST.encode(query) # embed new query
+        data = APPCFG.chroma_client.get_collection(name=APPCFG.collection_name)
+        scores, retrieved_examples = data.get_nearest_examples( # retrieve results
+            "embeddings", embedded_query, # compare our new embedded query with the dataset embeddings
+            k=k # get only top k results
+        )
+        return scores, retrieved_examples 
